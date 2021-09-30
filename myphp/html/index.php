b6015ce9f736ae10ef656f446daf7fa98c527c2d
ptquang2000 <ptquang2000@gmail.com>
2021-10-01 05:16:41 +0700

    End branch

--- myphp/html/index.php
+++ myphp/html/index.php
@@ -9,7 +9,10 @@ spl_autoload_register(function($class)
 {
   $parts = explode(DL, $class);
   $file = array_pop($parts);
-  $path = __DIR__.DL.implode(DL,$parts).DL.$file.'.php';
+  $parts = implode(DL,array_map(function($item){
+    return strtolower($item);
+  },$parts));
+  $path = __DIR__.DL.$parts.DL.$file.'.php';
   if (file_exists($path))
     include_once $path;
 });
@@ -17,14 +20,20 @@ spl_autoload_register(function($class)
 define('__STATIC__', 'app'.DL.'static'.DL);
 define('__TEMPLATE__', 'app'.DL.'templates'.DL);
 
+// configuration
+$config = parse_ini_file('config.ini');
+
 // Routing
 $url = $_SERVER['REQUEST_URI'];
 // $url = preg_replace('/(^\/)/', '', $url);
 $url = preg_replace('/(\\/?\?.*)/', '', $url);
 
 use Core\Router;
+
+// setup
 $df_app = glob("app/php/*.php");
 foreach ($df_app as $filename) 
   Router::setup($filename);
+
 Router::route($url);
 ?>

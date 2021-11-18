aeefaff43bcf2b102dc3de71470cf1cdeffd676b
nguyentrongkhanhduy <nguyentrongkhanhduy@gmail.com>
2021-11-18 11:49:23 +0700

    add some news

--- app/static/script/news.js
+++ app/static/script/news.js
@@ -3,3 +3,17 @@ a.addEventListener("click", (e) => {
   window.location.href = "/news/azul";
 });
 
+var b = document.getElementById("space");
+b.addEventListener("click", (e) => {
+  window.location.href = "/news/space";
+});
+
+var c = document.getElementById("sieuthu");
+c.addEventListener("click", (e) => {
+  window.location.href = "/news/sieuthu";
+});
+
+var d = document.getElementById("top10");
+d.addEventListener("click", (e) => {
+  window.location.href = "/news/top10";
+});

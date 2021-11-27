c17c986350e20e24596b7886d417349b523b3339
nguyentrongkhanhduy <nguyentrongkhanhduy@gmail.com>
2021-11-27 18:55:03 +0700

    add ads

--- app/static/script/ad.js
+++ app/static/script/ad.js
@@ -1,4 +1,21 @@
-var a = document.getElementById("avalon")
-a.addEventListener("click", (e) => {
-    window.location.href = "/product-detail?id=4";
+const news = {
+  avalon: "4",
+  matngu: "18",
+};
+
+for (const [key, value] of Object.entries(news)) {
+  var a = document.getElementById(`${key}`);
+  a.addEventListener("click", () => {
+    window.location.href = `/product-detail?id=${value}`;
   });
+}
+
+// var a = document.getElementById("avalon");
+// a.addEventListener("click", (e) => {
+//   window.location.href = "/product-detail?id=4";
+// });
+
+// var b = document.getElementById("matngu");
+// b.addEventListener("click", (e) => {
+//   window.location.href = "/product-detail?id=18";
+// });

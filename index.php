7a1fb862812ec8c64bab89de9e6cb4e34d3ede17
ptquang2000 <ptquang2000@gmail.com>
2021-11-28 23:54:50 +0700

    prefer current_url if access page required login

--- index.php
+++ index.php
@@ -2,14 +2,13 @@
 include_once 'core\\Attributes.php';
 include_once 'core\\Functions.php';
 include_once 'core\\Define.php';
-spl_autoload_register(function($class)
-{
+spl_autoload_register(function ($class) {
   $parts = explode(DL, $class);
   $file = array_pop($parts);
-  $parts = implode(DL,array_map(function($item){
+  $parts = implode(DL, array_map(function ($item) {
     return strtolower($item);
-  },$parts));
-  $path = __DIR__.DL.$parts.DL.$file.'.php';
+  }, $parts));
+  $path = __DIR__ . DL . $parts . DL . $file . '.php';
   if (file_exists($path))
     include_once $path;
 });
@@ -28,7 +27,6 @@ use Core\Router;
 
 // setup
 $df_app = glob("app/php/*.php");
-foreach ($df_app as $filename) 
+foreach ($df_app as $filename)
   Router::setup($filename);
 Router::route($url);
-?>

97484c29dbd5522b6fb74373f1f6c6cf6b3411a5
Tri Quang <ptquang2000@gmail.com>
2022-01-29 16:42:20 +0700

    update default get method

--- app/php/WebController.php
+++ app/php/WebController.php
@@ -5,20 +5,20 @@ use Core\{Controller, RequestMapping, RequestMethod, Model};
 
 #[Controller]
 class WebController {
-    #[RequestMapping(value: '/', method: RequestMethod::GET)]
+    #[RequestMapping(value: '/')]
     function get_home()
     {
         return 'home.html';
     }
-    #[RequestMapping(value: '/url', method: RequestMethod::GET)]
+    #[RequestMapping(value: '/url')]
     function get_static_url()
     {
         return 'static.html';
     }
-    #[RequestMapping(value: '/url/$number', method: RequestMethod::GET)]
+    #[RequestMapping(value: '/url/$number')]
     function get_dynamic_url($number, Model $model)
     {
-        if (is_numeric($number)) else die("invalid number");
+        if (is_numeric($number)) die("invalid number");
         $model->add_attribute("isEven", $number % 2 == 0);
         return 'dynamic.php';
     }

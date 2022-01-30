a8bf4a6c1e66e92a455093b4929c2bbf47b94406
Tri Quang <ptquang2000@gmail.com>
2022-01-30 21:11:56 +0700

    merge with main

--- core/RestControllerTrait.php
+++ core/RestControllerTrait.php
@@ -93,6 +93,12 @@ trait RestControllerTrait
 	}
 	public function __construct()
 	{
+		header("Access-Control-Allow-Origin: *");
+		header("Content-Type: application/json; charset=UTF-8");
+		header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+		header("Access-Control-Max-Age: 3600");
+		header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 		$reflection = new ReflectionClass(get_parent_class($this));
 		
 		// instantiate autwired properties;

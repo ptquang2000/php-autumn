34ca8960a919be47e1bbc52e24892ee64837f2cb
quang <ptquang200@gmail.com>
2022-01-31 09:14:32 +0700

    fix class directory separator

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include_once 'core/Attributes.php';
 include_once 'core/Functions.php';
 include_once 'core/Define.php';
 spl_autoload_register(function ($class) {
-  $parts = explode(DL, $class);
+  $parts = explode("\\", $class);
   $file = array_pop($parts);
   $parts = implode(DL, array_map(function ($item) {
     return strtolower($item);
